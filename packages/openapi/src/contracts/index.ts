8395ad38c852259229fa4746e753542e0370b446
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2026-05-11 22:03:41 +0530

    jwt token

--- packages/openapi/src/contracts/index.ts
+++ packages/openapi/src/contracts/index.ts
@@ -1,8 +1,10 @@
 import { initContract } from "@ts-rest/core";
+import { authContract } from "./auth.js";
 import { healthContract } from "./health.js";
 
 const c = initContract();
 
 export const apiContract = c.router({
+  Auth: authContract,
   Health: healthContract,
 });
